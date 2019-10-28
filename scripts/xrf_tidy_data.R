27caeda20e7fe68009a52dfb7ed12e2b548ff09f
Permalloo <per24n@csiro.au>
2019-10-28 17:19:13 +1100

    SUBSAMPLE_ID AND STEM_ID were added in the data files. Then a new "all_id_select" was created.

--- scripts/xrf_tidy_data.R
+++ scripts/xrf_tidy_data.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(readxl)
 
+##### Reading each file with sample IDs that was created each time when using pXRF #####
 xrf_id_22aug <-read_excel(
   "data/York_Grab_pXRF_IDs/York_Grab_pXRF_SampleID_22Aug19.xlsx",
   sheet = "22augID")
@@ -20,20 +21,23 @@ xrf_id_27aug <-read_excel(
 xrf_id_29aug <-read_excel(
   "data/York_Grab_pXRF_IDs/York_Grab_pXRF_SampleID_29Aug19.xlsx",
   sheet = "29augID")
+#------------------------------------------------------------------------------------#
 
+##### Join all sample IDs file into "all_id" #####
 all_id <- bind_rows(xrf_id_22aug, xrf_id_23aug, xrf_id_26aug, xrf_id_27aug, xrf_id_29aug)
 view(all_id)
 str(all_id)
 
+
+##### Select only where Remarks is equal to "NA" and GENPRINT not equal to "NA" #####
 all_id_select <- all_id %>% 
-  select(Trunc, GENPRINT, Date, Reading_No, Mode, Remarks) %>% 
+  select(Trunc, SUBSAMPLE_ID, STEM_ID, GENPRINT, Date, Reading_No, Mode, Remarks) %>% 
   filter(is.na(Remarks), !is.na(GENPRINT)) %>% 
   view  
 
-  view(all_id_select)
 
-  
-mutate(reading_id = str_pad(Reading_No, 3, "left", "0")) %>% 
+##### Changing the number format in column "Reading_No" to "000" and rename it as reading_id  
+all_id_select %>% mutate(reading_id = str_pad(Reading_No, 3, "left", "0")) %>% 
   unite(reading_id, Date, reading_id, sep = "-") %>% 
   group_by(Trunc)
 
